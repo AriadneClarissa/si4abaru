67bdaab48e31e6469f8dfd2dd4a06d2fec7107a5
AriadneClarissa <ariadneclarissadarnettaoswari_2327240056@mhs.mdp.ac.id>
2025-04-30 15:22:33 +0700

    Update index.blade.php

--- resources/views/fakultas/index.blade.php
+++ resources/views/fakultas/index.blade.php
@@ -1,5 +1,18 @@
 <h1>Fakultas</h1>
 
+<table>
+    <tr>
+        <th>nama</th>
+        <th>Singkatan</th>
+        <th>Dekan</th>
+        <th>Wakil Dekan</th>
+    </tr>
 @foreach ($fakultlas as $item)
+    <tr>
+        <td>{{ $item->nama }}</td>
+        <td>{{ $item->singkatan }}</td>
+        
+    </tr>
     {($item->nama )} {{$item->singkatan}} <br>
 @endforeach
+</table>
