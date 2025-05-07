e627e56b27d08ec434c36aea2cbd002013047b82
AriadneClarissa <ariadneclarissadarnettaoswari_2327240056@mhs.mdp.ac.id>
2025-05-07 15:04:00 +0700

    Menambah AdminLte4

--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,25 +1,56 @@
 @extends('layout.main')
+@section('title', 'Program Studi')
+    
 
 @section('content')
-    <h1>Program Studi</h1>
-    <table>
-        <tr>
-            <th>Nama</th>
-            <th>Singkatan</th>
-            <th>Kaprodi</th>
-            <th>Sekretaris</th>
-            <th>Fakultas</th>
-        </tr>
-        @foreach ($prodi as $item)
-            <tr>
-                <td>{{ $item->nama}}</td>
-                <td>{{ $item->singkatan}}</td>
-                <td>{{ $item->kaprodi}}</td>
-                <td>{{ $item->sekretaris}}</td>
-                <td>{{ $item->fakultas->nama}}</td>
-            </tr>
-        @endforeach
-    </table>
+<div class="card">
+    <div class="card-header">
+      <h3 class="card-title">List Prodi</h3>
+      <div class="card-tools">
+        <button
+          type="button"
+          class="btn btn-tool"
+          data-lte-toggle="card-collapse"
+          title="Collapse"
+        >
+          <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+          <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+        </button>
+        <button
+          type="button"
+          class="btn btn-tool"
+          data-lte-toggle="card-remove"
+          title="Remove"
+        >
+          <i class="bi bi-x-lg"></i>
+        </button>
+      </div>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nama</th>
+                    <th>Singkatan</th>
+                    <th>Kaprodi</th>
+                    <th>Sekretaris</th>
+                    <th>Fakultas</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($prodi as $item)
+                    <tr>
+                        <td>{{ $item->nama}}</td>
+                        <td>{{ $item->singkatan}}</td>
+                        <td>{{ $item->kaprodi}}</td>
+                        <td>{{ $item->sekretaris}}</td>
+                        <td>{{ $item->fakultas->nama}}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
 
     
